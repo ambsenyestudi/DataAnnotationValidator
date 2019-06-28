9e01183525df08e7d98176bcd311543516f55f6e
amb seny <adrifre7@hotmail.com>
2019-06-28 20:03:40 +0200

    Validator layout

---
+++ DataAnnotationValidator/ContactInfo/ContactnformationParser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAnnotationValidator.Domain.ContactInfo
+{
+    public class ContactnformationParser
+    {
+        public (bool, Contactnformation) TryParse(string  json, ILogger logger)
+        {
+            var success = false;
+            var result = default(Contactnformation);
+            //Todo parse data
+
+            return (success, result);
+        }
+    }
+}
