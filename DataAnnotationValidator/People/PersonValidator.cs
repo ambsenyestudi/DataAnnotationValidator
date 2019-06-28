ee8e2e33c42ce676ba798b787a75008d3353c21d
amb seny <adrifre7@hotmail.com>
2019-06-28 19:13:03 +0200

    Comments

--- DataAnnotationValidator/People/PersonValidator.cs
+++ DataAnnotationValidator/People/PersonValidator.cs
@@ -4,10 +4,13 @@ namespace DataAnnotationValidator.Domain.People
 {
     public class PersonValidator
     {
+        //Name Surname IdNumber required
+        //Email not required but if given must be correct
         public bool Validate(PersonInformation personInfo)
         {
             if (personInfo != null)
             {
+                
                 if(!string.IsNullOrEmpty(personInfo.Name)
                     && !string.IsNullOrEmpty(personInfo.Surname) 
                     && !string.IsNullOrEmpty(personInfo.IdNumber))
